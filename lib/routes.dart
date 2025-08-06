720b5ad5b4b6df343e70e02d568597afe2f5ab87
Jojo5673 <judoudo2005@gmail.com>
2025-08-06 02:11:53 -0500

    Moved Register Scaffold to Function for cleaner code

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:wheres_my_bus/driver/driver_home.dart';
 import 'package:wheres_my_bus/pages/landing_page.dart';
 import 'package:wheres_my_bus/pages/login.dart';
 import 'package:wheres_my_bus/pages/profile.dart';
-import 'package:wheres_my_bus/pages/sign_up.dart';
+import 'package:wheres_my_bus/pages/register.dart';
 import 'package:wheres_my_bus/passenger/passenger_home.dart';
 
 final router = GoRouter(
@@ -20,17 +19,12 @@ final router = GoRouter(
           path: '/forgot-password',
           builder: (context, state) {
             final arguments = state.uri.queryParameters;
-            return ForgotPasswordScreen(email: arguments['email'], headerMaxExtent: 200);
+            return ForgotPasswordScreen(email: arguments['email'], headerMaxExtent: 200);               
           },
         ),
       ],
     ),
-    GoRoute(
-      path: '/sign-up',
-      builder:
-          (context, state) =>
-              Scaffold(body: SignUp(context, state)), //makes keyboard not cover text fields
-    ),
+    GoRoute(path: '/sign-up', builder: (context, state) => Register(context, state)),
     GoRoute(path: '/profile', builder: (context, state) => Profile(context, state)),
     GoRoute(path: '/passenger', builder: (context, state) => PassengerHome()),
     GoRoute(path: '/driver', builder: (context, state) => DriverHome()),
